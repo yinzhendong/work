8a1efec81f3f3194b04d6564ae55a5a77ebf0192
yinzd <yinzd@boful.com>
2015-10-16 23:24:27 +0800

    modify doc

---
+++ newrms/enzod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+enzo_home=/root/enzo
+tomcat_home=/home/boful/tomcat
+
+# start tomcat
+cd $tomcat_home/bin
+./startup.sh
+
+# start enzo
+cd $enzo_home
+make venv
+source venv/bin/activate
+make deps
+nohup python manage.py runserver 0.0.0.0:8000 &
+
+# start redis
+nohup redis-server &
