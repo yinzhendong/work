ec98ed7c1747c28f824279a8eb3ef776f0f78e3e
yinzd <yinzd@boful.com>
2015-10-21 17:51:51 +0800

    update

--- script/updatewar.sh
+++ script/updatewar.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# loction
+# variable
 bcms_git=/root/bcms-master
 bcms_root=/home/boful/web
 tomcat_root=/home/boful/tomcat
+update_time=`date +%Y%m%d%H%M`
+source_ip=42.62.52.40
+target_ip=192.168.1.70
 
 # get bcms source 
 #cd $bcms_git
@@ -17,10 +20,12 @@ if [ -f "/root/master.zip" ]; then
 fi
 
 wget https://github.com/ligson/bcms/archive/master.zip
-unzip -q master.zip
+#unzip -q master.zip
+unzip  master.zip
 
 # modify config
-#cd $bcms_git/src/main/java/brms/action
+cd $bcms_git/src/main/java/brms/action
+sed -i "s/$source_ip/$target_ip/g" Proxy.java
 
 # make war
 cd $bcms_git
@@ -31,6 +36,12 @@ mvn war:war
 kill -9 `jps |grep Bootstrap |awk '{print $1}'`
 
 # replace bcms
+cd $bcms_root
+mv bcms bcmc.$update_time
+mkdir bcms
+cd bcms
+cp $bcms_git/target/bcms-1.0.war ./
+jar xvf bcms-1.0.war
 
 # start bcms
 cd $tomcat_root/bin
