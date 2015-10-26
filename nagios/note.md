f83dd431cc6b786db0f85180ead7da5a2bad2bca
yinzd <yinzd@boful.com>
2015-10-26 23:45:30 +0800

    update

---
+++ nagios/note.md
@@ -0,0 +1,4 @@
+#### install
+```
+wget http://nchc.dl.sourceforge.net/project/nagios/nagios-4.x/nagios-4.1.1/nagios-4.1.1.tar.gz
+```
--- newrms/enzod.sh
+++ newrms/enzod.sh
@@ -17,5 +17,7 @@ nohup redis-server &
 cd $enzo_home
 make venv
 source venv/bin/activate
+pip install django-annoying==0.8.2 django-debug-toolbar==1.3.2 django-mptt==0.7.1 \
+django-extensions==1.5.5 ipython==2.1.0 MySQL-python==1.2.5 pip==1.5.6
 make deps
 nohup python manage.py runserver 0.0.0.0:8000 &
