a8a20c73943becec8f39acd807adf706836bfa49
yinzd <yinzd@boful.com>
2015-10-30 21:59:20 +0800

    add handle mp4

--- script/conv_batch/conv_batch.sh
+++ script/conv_batch/conv_batch.sh
@@ -10,19 +10,19 @@ for file_path in `cat $file_name`; do
 	#echo ${file_name%.*}.mp4
 
 	# mediainfo get file vb/ab
-	file_vb_bps=`mediainfo --Output="Video;%BitRate%" $file_path`
+	file_vb_bps=`mediainfo --Output="Video;%BitRate%" "$file_path"`
 	#echo "${file_vb_bps}bps"
 	file_vb_kbps=`expr $file_vb_bps / 1000`
 	#echo "${file_vb_kbps}kbps"
 
-	file_ab_bps=`mediainfo --Output="Audio;%BitRate%" $file_path`
+	file_ab_bps=`mediainfo --Output="Audio;%BitRate%" "$file_path"`
 	#echo "${file_ab_bps}bps"
 	file_ab_kbps=`expr $file_ab_bps / 1000`
 	#echo "${file_ab_kbps}kbps"
 
-	file_width=`mediainfo --Output="Video;%Width%" $file_path`
+	file_width=`mediainfo --Output="Video;%Width%" "$file_path"`
 	#echo $file_width
-	file_height=`mediainfo --Output="Video;%Height%" $file_path`
+	file_height=`mediainfo --Output="Video;%Height%" "$file_path"`
 	#echo $file_height
 
 	# use HandBrake conv file to mp4
--- script/handle_mp4/handle_mp4.sh
+++ script/handle_mp4/handle_mp4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+transcode_root=/home/media/video
+
 cat out | while read LINE; do
 	echo "file_hash+file_path is --> $LINE"
 	file_hash=`echo $LINE | cut -d ':' -f 1`
@@ -8,6 +10,25 @@ cat out | while read LINE; do
 	file_path=`echo $LINE | cut -d ':' -f 2`
 	echo "file_path is --> $file_path"
 
-	echo "cp \"$file_path\" /home/store/$file_hash.mp4"
+	file_name=${file_path##*/}
+	echo $file_name
+
+	file_height=`mediainfo --Output="Video;%Height%" "$file_path"`
+	echo $file_height	
+
+	file_width=`mediainfo --Output="Video;%Width%" "$file_path"`
+	echo $file_width
+
+	#file_size=`mediainfo --Output="General;%FileSize%" "$file_path"`
+	#echo $file_size
+
+	#file_duration=`mediainfo --Output="General;%Duration%" "$file_path"`
+	#echo $file_duration
+
+	cd $transcode_root
+	mkdir $file_hash	
+	echo "cp \"$file_path\" \"$transcode_root/$file_hash/${file_width}_${file_height}.mp4\""
+	`cp "$file_path" "$transcode_root/$file_hash/${file_width}_${file_height}.mp4"`
+	
 	echo "--------------"
 done
