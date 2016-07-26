ec76cfdea91853dee3a73cb3fae7a981f4b53232
yinzd <yinzd@boful.com>
2016-07-27 00:12:52 +0800

    modified mysql_auto_backup.sh

--- shell/mysql_auto_backup.sh
+++ shell/mysql_auto_backup.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 # mysql auto backup with crontab
 
-#date "+%y%m%d_%H%M%S"
+dbuser=root
+dbpasswd=root
+dbname=enzo
+backup_path=/backup1
+#backup_file_name=`date "+%Y%m%d_%H%M%S"`
+backup_file_name=`date "+%Y%m%d"`.${dbname}
 
-backup_file_name=`date "+%y%m%d_%H%M%S"`
-
-#echo $backup_file_name
-#echo ./${backup_file_name}.sql
-
-#echo /mnt/${backup_file_name}.sql
-
-echo "Backup......"
-mysqldump -u root -ppassword convtust > /mnt/${backup_file_name}.sql
-echo "Backup Done!"
+#echo "Backup......"
+mysqldump -u${dbuser} -p${dbpasswd} ${dbname} > ${backup_path}/${backup_file_name}.sql
+#echo "Backup Done!"
 
 
-echo "Zipping File......"
-tar zcvf /mnt/${backup_file_name}.tar.gz ${backup_file_name}.sql
+#echo "Zipping File......"
+tar -zcf ${backup_path}/${backup_file_name}.tar.gz ${backup_path}/${backup_file_name}.sql
 
-rm -rf /mnt/${backup_file_name}.sql
+#rm -rf ${bakcup_path}${backup_file_name}.sql
 
-echo "saving logs......"
+#echo "Saving logs......"
 
-echo ${backup_file_name}.tar.gz >> /mnt/dblog
+echo ${backup_file_name}.tar.gz >> ${backup_path}/log
 
-echo "done!"
-
-echo "Total backup files is:`cat /mnt/dblog | wc -l`"
-
-#------------delete older backup files---------------------
-#
-#echo "delete older 10 files......"
-#for ((i=0; i<10; i++))
-#do
-#	rm -rf /mnt/`cat /mnt/dblog | awk '{print $0}'`
-#done
-#echo "Delete done!"
+#echo "Done!"
 
